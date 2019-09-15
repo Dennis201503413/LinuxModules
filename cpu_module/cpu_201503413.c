4948fdb688e988fc4c403bab86703ca24958b8b1
Dennis201503413 <dennismasaya@gmail.com>
2019-09-15 17:44:04 -0600

    modules working correctly

--- cpu_module/cpu_201503413.c
+++ cpu_module/cpu_201503413.c
@@ -1,11 +1,103 @@
-#include<linux/module.h> //needs to be included for every module
-#include<linux/kernel.h> //required for macro expansion of printk
-int init_module(void) //executed when module is loaded into the kernel
-{
- printk(KERN_INFO “CARNET: 201503413 \n”);
- return 0;
+/*
+--------------------------------
+Developed by                    |
+user: Dennis Masaya             |
+id: 201503413                   |
+e-mail: dennismasaya@gmail.com  |
+--------------------------------
+This module demonstrates on how to build a module that displays
+information about processes creating a file in the /proc directory.
+*/
+
+/*  LIBRARY IMPORTS  */
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/sched.h>
+#include <linux/sched/signal.h>
+#include <linux/module.h>
+#include <linux/seq_file.h>
+#include <linux/proc_fs.h>
+
+/*  MODULE INFO  */
+MODULE_AUTHOR("Dennis Masaya");
+MODULE_DESCRIPTION("This module demonstrates on how to build a module that displays information about processes creating a file in the /proc directory.");
+MODULE_LICENSE("GPL");
+
+static struct proc_dir_entry *ent;
+
+static int proc_show(struct seq_file *m, void *v) {
+    int cont = 0;
+    struct task_struct *task;
+    seq_printf(m,"%10d %-30s %-30s\n","PID","NAME","STATE");
+
+    for_each_process(task){
+      if (task->state == TASK_RUNNING) {
+        seq_printf(m,"%10d %-30s %-30s\n",task->pid,task->comm,"TASK_RUNNING");
+      }
+      else if (task->state == TASK_INTERRUPTIBLE) {
+        seq_printf(m,"%10d %-30s %-30s\n",task->pid,task->comm,"TASK_INTERRUPTIBLE");
+      }
+      else if (task->state == TASK_UNINTERRUPTIBLE) {
+        seq_printf(m,"%10d %-30s %-30s\n",task->pid,task->comm,"TASK_UNINTERRUPTIBLE");
+      }
+      else if (task->state == __TASK_STOPPED) {
+        seq_printf(m,"%10d %-30s %-30s\n",task->pid,task->comm,"TASK_STOPPED");
+      }
+      else if (task->state == __TASK_TRACED) {
+        seq_printf(m,"%10d %-30s %-30s\n",task->pid,task->comm,"TASK_TRACED");
+      }
+      else if (task->exit_state == EXIT_DEAD) {
+        seq_printf(m,"%10d %-30s %-30s\n",task->pid,task->comm,"EXIT_DEAD");
+      }
+      else if (task->exit_state == EXIT_ZOMBIE) {
+        seq_printf(m,"%10d %-30s %-30s\n",task->pid,task->comm,"EXIT_ZOMBIE");
+      }
+      else if (task->state == TASK_DEAD) {
+        seq_printf(m,"%10d %-30s %-30s\n",task->pid,task->comm,"TASK_DEAD");
+      }
+      else if (task->state == TASK_NOLOAD) {
+        seq_printf(m,"%10d %-30s %-30s\n",task->pid,task->comm,"TASK_NOLOAD");
+      }
+      else if (task->state == TASK_WAKEKILL) {
+        seq_printf(m,"%10d %-30s %-30s\n",task->pid,task->comm,"TASK_WAKEKILL");
+      }
+      else if (task->state == TASK_WAKING) {
+        seq_printf(m,"%10d %-30s %-30s\n",task->pid,task->comm,"TASK_WAKING");
+      }
+
+
+
+      cont = cont + 1;
+    }
+
+    return 0;
 }
-void cleanup_module(void) //executed as the kernel module is removed from the kernel
+
+static void __exit end(void)
 {
- printk(KERN_INFO “CURSO: Sistemas Operativos 1 \n”);
+  proc_remove(ent);
+  printk(KERN_INFO "CURSO: Sistemas Operativos 1 \n");
 }
+
+static int proc_open(struct inode *inode, struct file *file) {
+    return single_open(file, proc_show, NULL);
+}
+
+static const struct file_operations proc_fops = {
+    .open = proc_open,
+    .read = seq_read,
+    .llseek = seq_lseek,
+    .release = single_release,
+};
+
+static int __init start(void) 
+{
+    printk(KERN_INFO "CARNET: 201503413 \n");
+    ent = proc_create("cpu_201503413", 0, NULL, &proc_fops);
+    return 0;
+}
+
+
+module_init(start);
+module_exit(end);
--- mem_module/memo_201503413.c
+++ mem_module/memo_201503413.c
@@ -1,56 +1,96 @@
-#include <linux/module.h> //needs to be included for every module
-#include <linux/kernel.h> //required for macro expansion of printk
-#include <linux/moduleparam.h>
-#include <linux/init.h>
-#include <linux/proc_fs.h>
-#include <asm/uaccess.h>
+/*
+--------------------------------
+Developed by                    |
+user: Dennis Masaya             |
+id: 201503413                   |
+e-mail: dennismasaya@gmail.com  |
+--------------------------------
+This module demonstrates on how to build a module that displays
+information about memory creating a file in the /proc directory.
+*/
 
+/*  LIBRARY IMPORTS  */
 #include <linux/fs.h>
-#include <linux/hugetlb.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
 #include <linux/mm.h>
+#include <linux/hugetlb.h>
 #include <linux/mman.h>
 #include <linux/mmzone.h>
+#include <linux/module.h>
+#include <linux/proc_fs.h>
 #include <linux/quicklist.h>
 #include <linux/seq_file.h>
 #include <linux/swap.h>
 #include <linux/vmstat.h>
 #include <linux/atomic.h>
+#include <linux/vmalloc.h>
 #include <asm/page.h>
 #include <asm/pgtable.h>
+#include <asm/uaccess.h>
+#include <linux/sched.h>
+#include <linux/cpumask.h>
+#include <linux/interrupt.h>
+#include <linux/kernel_stat.h>
+#include <linux/slab.h>
+#include <linux/time.h>
+#include <linux/irqnr.h>
+#include <linux/tick.h>
+#include <asm/apic.h>
+#include <linux/smp.h>
+#include <linux/timex.h>
+#include <linux/string.h>
+#include <linux/cpufreq.h>
+#include <linux/delay.h>
 
-#define BUFSIZE 100
-
+/*  MODULE INFO  */
 MODULE_AUTHOR("Dennis Masaya");
-struct sysinfo i;
+MODULE_DESCRIPTION("This module demonstrates on how to build a module that displays information about memory creating a file in the /proc directory.");
+MODULE_LICENSE("GPL");
 
+struct sysinfo i;
 static struct proc_dir_entry *ent;
 
-static ssize_t mywrite(struct file *file, const char __user *ubuf, size_t count, loff_t *ppos){
-  printk(KERN_DEBUG "write handler\n");
-  printk(KERN_DEBUG "MemTotal:       %8lu kB\n",i.totalhigh);
-  return -1;
-}
-
-static ssize_t myread(struct file *file, char __user *ubuf, size_t count, loff_t *ppos){
-  printk(KERN_DEBUG "read handler\n");
-  return 0;
-}
-
-static struct file_operations myops =
+static int mem_info(struct seq_file *m, void *v)
 {
-  .owner = THIS_MODULE,
-  .read = myread,
-  .write = mywrite,
-};
+  // Se accede a la estructura sysinfo y se cargan sus atributos
+  si_meminfo(&i);
 
-int init_module(void) //executed when module is loaded into the kernel
-{
- printk(KERN_INFO "CARNET: 201503413 \n");
- ent = proc_create("memo_201503413",0660,NULL,&myops);
- return 0;
+  #define Ajuste(x) ((x) << (PAGE_SHIFT - 10))
+      seq_printf(m,"Total memory: %d \n", Ajuste(i.totalram));
+      seq_printf(m,"Free memory: %d \n", Ajuste(i.freeram));
+      seq_printf(m,"Used memory percentage: %d%% \n", (100 - Ajuste(i.freeram)*100/Ajuste(i.totalram)));
+  #undef Ajuste
+      return 0;
+
 }
-void cleanup_module(void) //executed as the kernel module is removed from the kernel
+
+static void __exit end(void)
 {
   proc_remove(ent);
- printk(KERN_INFO "CURSO: Sistemas Operativos 1 \n");
+  printk(KERN_INFO "CURSO: Sistemas Operativos 1 \n");
 }
+
+static int mem_info_open(struct inode *inode, struct file *file)
+{
+    return single_open(file, mem_info, NULL);
+}
+
+static const struct file_operations mem_info_fops = {
+    .open		= mem_info_open,
+    .read		= seq_read,
+    .llseek		= seq_lseek,
+    .release	= single_release,
+};
+
+
+static int __init start(void)
+{
+    printk(KERN_INFO "CARNET: 201503413 \n");
+    ent = proc_create("memo_201503413", 0, NULL, &mem_info_fops);
+    return 0;
+}
+
+
+module_init(start);
+module_exit(end);
